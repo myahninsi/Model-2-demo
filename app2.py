0ed272c45117e78632eae7e465b91ab1df91f5ae
myahninsi <157079268+myahninsi@users.noreply.github.com>
2024-08-07 06:15:50 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from sklearn.model_selection import TimeSeriesSplit
